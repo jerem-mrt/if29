e553a17e79a99e9faff7598b77c671d64f7dd70d
Poignant <elise.poignant@utt.fr>
2020-06-05 14:28:26 +0200

    visibilité : sauvegarde dans user

--- visibilite.py
+++ visibilite.py
@@ -7,15 +7,24 @@ import numpy as np
 #lien avec mongoDB
 client = MongoClient('localhost', 27017)
 db = client.if29
-collection = db.tweet
+tweet = db.tweet
+
+#lien avec la collection user
+user = db.user
+
 i=0
+n=0
 taille_moy_mention = 11.4
 float(taille_moy_mention)
 taille_moy_tag = 11.6
 float(taille_moy_tag)
+user_id_meme=5549
+n=0
+moyenne_visibilite=0
+
 
 #récupération nombre de # et de mention 
-for x in collection.find({},{"entities.hashtags":1,"entities.user_mentions":1, "_id":0}):
+for x in tweet.find({},{"entities.hashtags":1,"entities.user_mentions":1, "_id":0, "user.id":1}).sort("user.id",1):
     print("Tweet numero %d"%(i))
     i+=1
     print("Nombre de hashtags : ")
@@ -30,5 +39,14 @@ for x in collection.find({},{"entities.hashtags":1,"entities.user_mentions":1, "
     print("Visibilité du tweet : ")
     visibilite = (mention_tweet * taille_moy_mention + tag_tweet * taille_moy_tag)/140
     print(visibilite)
-
-
+    #récupération de l'id de l'utilisateur
+    user_id = x.get("user").get("id")
+    print(user_id)
+    if user_id==user_id_meme:
+        n=n+1
+        user_id_meme = user_id
+        moyenne_vibilite = (visibilite + moyenne_visibilite)/n
+        user.insert_many({'id':user_id , "visibilite":moyenne_vibilite})
+        print('done')
+    else:
+        print('user suivant')
