aad8664ebced4fd45ff95aa641a9defeae339d6f
Callista <spitericallista@gmail.com>
2020-06-30 11:49:24 +0200

    Update svm_agr_visi.py

--- labels/svm_agr_visi.py
+++ labels/svm_agr_visi.py
@@ -15,7 +15,7 @@ for info in user.find({"label_agr_visi": {"$exists":"true"}},{"agressivity":1, "
     x=[[info.get("PC1"), info.get("PC2")]]
     X=X+x
     y.append(info.get("label_agr_visi"))
-#On converti X en np array pour mieux l'utiliser après
+#On converti X en np array pour mieux l'utiliser apres
 X=np.array(X)
 y=np.array(y)
 #definition du calassifier
@@ -28,10 +28,10 @@ a = -w[0] / w[1]
 xx = np.linspace(0,12)
 yy = a * xx - clf.intercept_[0] / w[1]
 
-#séparateur
+#separateur
 h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
 
-# points avec différentes couleurs
+# points avec differentes couleurs
 plt.scatter(X[:, 0], X[:, 1], c = y)
 plt.legend()
 plt.show()
